f1d545c730509a16c5b1b033ff5eb5d150708129
Pierre Boucher <pboucher@2F2.42angouleme.fr>
2025-01-23 15:07:06 +0100

    all

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pboucher <pboucher@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 23:02:19 by pboucher          #+#    #+#             */
+/*   Updated: 2024/10/12 23:02:19 by pboucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (lst && del)
+	{
+		while (*lst)
+		{
+			temp = (*lst)->next;
+			del((*lst)->content);
+			free(*lst);
+			(*lst) = temp;
+		}
+	}
+}
---
+++ ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pboucher <pboucher@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 12:21:54 by pboucher          #+#    #+#             */
+/*   Updated: 2024/10/13 16:09:27 by pboucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	if (dest >= src)
+	{
+		while (n-- > 0)
+			*(unsigned char *)(dest + n) = *(unsigned char *)(src + n);
+	}
+	else
+	{
+		while (i < n)
+		{
+			*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
+			i++;
+		}
+	}
+	return (dest);
+}
